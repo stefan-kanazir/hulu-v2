e96660769652fe59ebdf0d935298bdf8852890e4
stefan-kanazir <kanazir.stefan@gmail.com>
2021-09-18 22:01:37 +0200

    Nav - finished

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,19 @@
 import requests from "../utils/requests";
+import router, { useRouter } from "next/router";
 
 function Nav() {
     return (
-        <nav>
-            <div className="flex px-10 sm:px-20 overflow-x-scroll text-2xl whitespace-nowrap space-x-10 sm:space-x-20 scrollbar-hide">
+        <nav className="relative px-1 w-10/12 mx-auto">
+            <div className="flex pr-10 sm:pr-20 overflow-x-scroll text-2xl whitespace-nowrap space-x-10 sm:space-x-20 scrollbar-hide">
                 {Object.entries(requests).map(([key, { title, url }]) => (
-                    <h2 key={key} className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500">{title}</h2>
+                    <h2 
+                        key={key}
+                        onClick={() => router.push(`/?genre=${key}`)} 
+                        className="cursor-pointer transition duration-100 transform hover:text-white hover:underline active:text-red-500"
+                    >{title}</h2>
                 ))}
             </div>
+            <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"></div>
         </nav>
     )
 }
